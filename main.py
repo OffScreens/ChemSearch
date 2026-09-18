6f54b07d189da01bf0c8948a0b2e4d5edca4df48
OffScreens <offscreensdev@gmail.com>
2026-09-18 23:15:50 +0200

    Added environment.yml for dependencies

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import requests # necesario para las API requests
 
-# 1. Definimos la dirección exacta de lo que queremos buscar
+# Dirección de lo que queremos buscar
 url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/aspirin/property/MolecularFormula,MolecularWeight/JSON"
 
-# 2. El "mensajero" va a esa dirección y trae la respuesta
+# Buscamos la respuesta de la búsqueda
 respuesta = requests.get(url)
 
-# 3. Convertimos esa respuesta de texto a un diccionario de Python
+# Convertimos esa respuesta de texto a un diccionario de Python
 datos = respuesta.json()
-
-# 4. Mostramos todo el paquete en la terminal
 print(datos)
