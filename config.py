4ded57ca9f38c3884a41e4a2005f9189844a711d
OffScreens <offscreensdev@gmail.com>
2026-09-19 16:51:38 +0200

    Added support for more in-depth information about chemical compounds, for now, only JSON format

--- config.py
+++ config.py
@@ -1,9 +1,26 @@
 
 # Returns; MolecularWeight, IUPACName and CID number
-PUBCHEM_PUG = "https://pubchem.ncbi.nlm.nih.gov/rest/pug"
+PUBCHEM_PUG = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/"
 
-def get_compound_name(compound_name):
-    return f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{compound_name}/property/IUPACName,MolecularWeight/JSON"
+def get_compound_base_info(compound_name):
+    return f"{PUBCHEM_PUG}compound/name/{compound_name}/property/IUPACName,MolecularWeight/JSON"
 
 # Returns;
-PUBCHEM_PUG_VIEW = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view"
+PUBCHEM_PUG_VIEW = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound"
+
+# For the sake of not repeating code, we create a "dynamic" link
+PUG_VIEW_HEADERS = [
+    "GHS+Classification",
+    "Toxicological+Information",
+    "Skin+Corrosion%2FIrritation",
+    "Reactivity+Profile",
+    "Storage+Conditions",
+    "Personal+Protective+Equipment+(PPE)",
+]
+# For PUG_VIEW API we need the CID, cannot use name, so a function is created to convert name to CID
+# Complemented by function get_cid_by_name in main.py
+def get_cid_url(compound_name):
+    return f"{PUBCHEM_PUG}/compound/name/{compound_name}/cids/JSON"
+
+def get_specific_info(cid, header):
+    return f"{PUBCHEM_PUG_VIEW}/{cid}/JSON?heading={header}"
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-import requests # necesario para las API requests
+import time
+import requests
 import config
 
-def search_compound(compund_name):
-    api_request = config.get_compound_name(compound_name)
+def search_basic_info(compound_name): # Outputs basic PUG information
+    api_request = config.get_compound_base_info(compound_name)
     data = requests.get(api_request)
     print(data.json())
 
+def get_cid_by_name(compound_name): # Gets the compound CID identifier by its name (necessary for search_deep_info)
+    cid_url = config.get_cid_url(compound_name)
+    cid = requests.get(cid_url)
+
+    if cid.status_code == 200: # If PubChem gives a positive response
+        cid_number = cid.json()
+        print("Compound identified")
+        return cid_number["IdentifierList"]["CID"][0] # Grab the CID number
+    else:
+        print(f"No such compound by the name {compound_name}")
+
+def search_deep_info(cid):
+    for header in config.PUG_VIEW_HEADERS:
+        deep_api_request = config.get_specific_info(cid, header)
+        time.sleep(0.2)
+        deep_info = requests.get(deep_api_request)
+        print(deep_info.json())
+
 compound_name = input("Please enter the name of the compound you want to search: ")
-search_compound(compound_name)
+cid = get_cid_by_name(compound_name)
+search_basic_info(compound_name)
+search_deep_info(cid)
