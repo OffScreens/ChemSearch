34446b62151188a5784df222460cb8abb39ba724
OffScreens <offscreensdev@gmail.com>
2026-09-22 23:26:11 +0200

    Deprecated get_cid_by_name() function, same functionality with only basic info request

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time, requests, config, json
+import config
+import requests
+import time
 
 # Read user_agent.txt with good format so session.headers doesn't crap itself
 with open("user_agent.txt", "r", encoding="utf-8") as user_file:
@@ -14,17 +16,6 @@ def search_basic_info(compound_name): # Outputs basic PUG information
     api_request = config.get_compound_base_info(compound_name)
     basic_data = session.get(api_request)
     return basic_data.json()
-
-def get_cid_by_name(compound_name): # Gets the compound CID identifier by its name (necessary for search_deep_info)
-    cid_url = config.get_cid_url(compound_name)
-    cid = session.get(cid_url)
-
-    if cid.status_code == 200: # If PubChem gives a positive response
-        cid_number = cid.json()
-        print("Compound identified")
-        return cid_number["IdentifierList"]["CID"][0] # Grab the CID number
-    else:
-        print(f"No such compound by the name {compound_name}")
 
 def search_deep_info(cid):
     for header in config.PUG_VIEW_HEADERS:
@@ -62,4 +53,4 @@ print(f"  Molecular weight: {molecular_weight}")
 time.sleep(1)
 #search_basic_info(compound_name)
 time.sleep(1)
-#search_deep_info(cid)
+search_deep_info(cid_number)
