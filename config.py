d3dbbf36ad84035dc0c483c46937a375000d36c2
OffScreens <offscreensdev@gmail.com>
2026-09-22 13:13:19 +0200

    Adjusted request timings

--- config.py
+++ config.py
@@ -11,11 +11,11 @@ PUBCHEM_PUG_VIEW = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound
 # For the sake of not repeating code, we create a "dynamic" link
 PUG_VIEW_HEADERS = [
     "GHS+Classification",
-    "Toxicological+Information",
-    "Skin+Corrosion%2FIrritation",
-    "Reactivity+Profile",
-    "Storage+Conditions",
-    "Personal+Protective+Equipment+(PPE)",
+#    "Toxicological+Information",
+#    "Skin+Corrosion%2FIrritation",
+#    "Reactivity+Profile",
+#    "Storage+Conditions",
+#    "Personal+Protective+Equipment+(PPE)",
 ]
 # For PUG_VIEW API we need the CID, cannot use name, so a function is created to convert name to CID
 # Complemented by function get_cid_by_name in main.py
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time, requests, config
+import time, requests, config, json
 
-# Read user_agent.txt with good format so session.headers.update doesn't crap itself
+# Read user_agent.txt with good format so session.headers doesn't crap itself
 with open("user_agent.txt", "r", encoding="utf-8") as user_file:
     user_agent = user_file.read().rstrip()
 print(user_agent)
@@ -29,7 +29,7 @@ def get_cid_by_name(compound_name): # Gets the compound CID identifier by its na
 def search_deep_info(cid):
     for header in config.PUG_VIEW_HEADERS:
         deep_api_request = config.get_specific_info(cid, header)
-        time.sleep(0.25)
+        time.sleep(0.5)
         deep_info = session.get(deep_api_request)
         print(deep_info.json())
 
@@ -41,9 +41,9 @@ def check_connection_status():
 # MAIN APP*
 compound_name = input("Please enter the name of the compound you want to search: ")
 check_connection_status()
-time.sleep(0.5)
+time.sleep(0.75)
 cid = get_cid_by_name(compound_name)
-time.sleep(0.5)
+time.sleep(0.75)
 search_basic_info(compound_name)
-time.sleep(0.5)
+time.sleep(0.75)
 search_deep_info(cid)
