bffac381ec0ec370b62c72680af652b32bb26109
OffScreens <offscreensdev@gmail.com>
2026-09-24 00:35:37 +0200

    More work into parsing

--- info_parsing.py
+++ info_parsing.py
@@ -2,6 +2,11 @@ import main
 
 def ghs_parsing(ghs_data):
     record = main.ghs_info.get("Record", {})
+    sections = record.get("Section", [])
+    sub_section = sections.get("Section", [])
+    information = sub_section.get("Information", [])
+    for section in sections:
+
 
 
 # Lots of work to be done
