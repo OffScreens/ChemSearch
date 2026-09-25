0d06ec6f3d27c7b4446f793e5ffb9ed14e528196
OffScreens <offscreensdev@gmail.com>
2026-09-26 00:07:25 +0200

    Added support for clean GHS printing WITH SAMPLE DATA, started work on doing it with user's search

--- info_parsing.py
+++ info_parsing.py
@@ -1,4 +1,4 @@
-import main
+#import main
 import json
 from pathlib import Path
 
@@ -7,12 +7,40 @@ json_path = Path("sample_data/GHSaspirin.json")
 with open(json_path, "r", encoding="utf-8") as f:
     datos = json.load(f)
 
-def ghs_parsing(ghs_data):
-    record = main.ghs_info.get("Record", {})
-    sections = record.get("Section", [])
-    sub_section = sections.get("Section", [])
-    information = sub_section.get("Information", [])
+#print(datos)
+
+def ghs_parsing(datos):
+#    record = main.ghs_info.get("Record", {})
+    record = datos.get("Record", {}) # Dictionary, we use .get
+    sections = record.get("Section", []) # List, use for loop
     for section in sections:
+        if section.get("TOCHeading") == "Safety and Hazards":
+            hazards_sections = section.get("Section", [])
+            for hazard_section in hazards_sections:
+                if hazard_section.get("TOCHeading") == "Hazards Identification":
+                    ghs_sections = hazard_section.get("Section", [])
+                    for ghs_section in ghs_sections:
+                        if ghs_section.get("TOCHeading") == "GHS Classification":
+                            information = ghs_section.get("Information", [])
+                            for info in information:
+                                if info.get("Name") == "GHS Hazard Statements":
+                                    values = info.get("Value", {})
+                                    markup_list = values.get("StringWithMarkup", [])
+                                    # Now inside of "GHS Hazard Statements", search
+                                    # recursively for all statements
+                                    for statement in markup_list:
+                                        text = statement.get("String")
+                                        print(f"- {text}")
+
+
+
+
+
+    return []
+    # Notice that there isn't any variable storing GHS statements, maybe for later.
+
+ghs_parsing(datos)
+
 
 
 
