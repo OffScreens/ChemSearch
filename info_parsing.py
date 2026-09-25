24cbb5ec89079941fb74fd2fbdc0c545ab8b3201
OffScreens <offscreensdev@gmail.com>
2026-09-25 19:19:02 +0200

    Sample json data

--- info_parsing.py
+++ info_parsing.py
@@ -1,4 +1,11 @@
 import main
+import json
+from pathlib import Path
+
+# This is just for testing purposes, change for definitive with ghs_data or json.
+json_path = Path("sample_data/GHSaspirin.json")
+with open(json_path, "r", encoding="utf-8") as f:
+    datos = json.load(f)
 
 def ghs_parsing(ghs_data):
     record = main.ghs_info.get("Record", {})
@@ -6,6 +13,7 @@ def ghs_parsing(ghs_data):
     sub_section = sections.get("Section", [])
     information = sub_section.get("Information", [])
     for section in sections:
+
 
 
 
